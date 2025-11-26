3551d2e61d8be8f6df706978f070f81d7aafe451
nidhiii <nidhiyogeesha12@gmail.com>
2025-11-27 00:07:24 +0530

    Update simulate_dns_flood.py

--- simulate_dns_flood.py
+++ simulate_dns_flood.py
@@ -3,9 +3,9 @@ import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-for i in range(40):
-    msg = "DNS FLOOD ATTEMPT"
-    sock.sendto(msg.encode(), ("127.0.0.1", 9999))
+for i in range(50):  # 50 flood packets
+    message = "SIMULATED DNS FLOOD"
+    sock.sendto(message.encode(), ("127.0.0.1", 9999))
     time.sleep(0.03)
 
 print("DNS flood simulation complete.")
